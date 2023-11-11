983d43f4ee1da8311ac4ca8d181823617619f42e
nubcoder69 <pb.b318@hotmail.com>
2023-11-11 21:48:08 +0530

    added new code changes

--- app/src/main/java/com/appx/elementcraft/TickBox.java
+++ app/src/main/java/com/appx/elementcraft/TickBox.java
@@ -1,4 +1,28 @@
 package com.appx.elementcraft;
 
-public class TickBox {
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class TickBox extends LinearLayout
+{
+    public TickBox(@NonNull Context context)
+    {
+        super(context);
+        init(context);
+    }
+
+    public TickBox(@NonNull Context context, @Nullable AttributeSet attrs)
+    {
+        super(context, attrs);
+        init(context);
+    }
+
+    private void init(Context context)
+    {
+        inflate(context,R.layout.tick_box,this);
+
+    }
 }
